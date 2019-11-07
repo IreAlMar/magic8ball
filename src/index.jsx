c371fc38cd2f33095ddcc54697725a5bdecff681
Irene <irodri26@its.jnj.com>
2019-11-07 15:47:53 +0000

    Add: ESLint linter

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,8 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MOUNT_ELEMENT = document.getElementById('root');
+ReactDOM.render(<App />, MOUNT_ELEMENT);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
